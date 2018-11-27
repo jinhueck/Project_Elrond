6851df22a008f8531d5d4f410f4de349eb3c76fb
burgii <qorgh9184@gmail.com>
2018-11-27 16:37:41 +0900

    UI 관련

---
+++ Assets/Script/JinHyeok/Test/Test.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour {
+
+    public uTools.TweenScale tweenScale;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void Awake()
+    {
+        btnPause.onClick.AddListener(OnClick_Pause);
+        uiPause.Close();
+    }
+
+    public UIPause uiPause;
+    public Button btnPause;
+
+    public void OnClick_Pause()
+    {
+        uiPause.Open();
+    }
+}
---
+++ Assets/Script/JinHyeok/UIPause.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPause : MonoBehaviour {
+
+    public void OpenMainScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("JungHwan");
+    }
+
+	public void Open()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Close()
+    {
+        gameObject.SetActive(false);
+        
+    }
+}
