e50b0c64ec26c6c78a2d961b88f21bc10145bc09
burgii <qorgh9184@gmail.com>
2018-11-28 18:04:56 +0900

    Ruby기능 추가

---
+++ Assets/Script/JinHyeok/Tile/Ruby_Script.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ruby_Script : Tile_Script {
+
+    protected override void SetBool()
+    {
+        isTouched = false;
+    }
+
+    public override void Touched()
+    {
+        if (isTouched == false)
+        {
+            isTouched = true;
+            ImageChange(tileSprite[1]);
+            InGameManager.instance.AddScore();
+            InGameManager.instance.Jewelry();
+            Sound_Script.instance.Play_EffectPopSound();
+            transform.parent.GetComponent<BombManager>().CheckIsTouched();
+        }
+    }
+}
