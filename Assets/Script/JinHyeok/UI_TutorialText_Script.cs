60da914648da84cd285839e6c04cb936c8db06c6
burgii <qorgh9184@gmail.com>
2018-12-14 20:24:32 +0900

    완성!

--- Assets/Script/JinHyeok/UI_TutorialText_Script.cs
+++ Assets/Script/JinHyeok/UI_TutorialText_Script.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 
 public class UI_TutorialText_Script : MonoBehaviour {
 
+    [SerializeField] Delegate_Script delegate_Script;
+    [SerializeField] private GameObject ui_Langauge;
+
     [SerializeField] string key_Langauge = "Set_Language";
     [SerializeField] int num_Language;
 
@@ -17,29 +20,54 @@ public class UI_TutorialText_Script : MonoBehaviour {
 
     public void Setup()
     {
-        if(PlayerPrefs.HasKey(key_Langauge))
+        delegate_Script = GetComponent<Delegate_Script>();
+        delegate_Script.SetTarget(ui_Langauge);
+
+        if (PlayerPrefs.HasKey(key_Langauge))
         {
             num_Language = PlayerPrefs.GetInt(key_Langauge);
         }
         else
         {
-            num_Language = 0;
+            string country = Application.systemLanguage.ToString();
+            switch(country)
+            {
+                case "Korean":
+                    num_Language = 0;
+                    break;
+                case "English":
+                    num_Language = 1;
+                    break;
+            }
             PlayerPrefs.SetInt(key_Langauge, num_Language);
         }
         SetText(num_Language);
     }
-    [SerializeField] string[] value;
-    [SerializeField] TextAsset text;
-    void SetText(int num)
+
+    public void SetText(int num)
     {
         num_Language = num;
         PlayerPrefs.SetInt(key_Langauge, num_Language);
-        text = Resources.Load<TextAsset>("JinHyeok/Language_Info/" + num_Language);
+        TextAsset text = Resources.Load<TextAsset>("JinHyeok/Language_Info/" + num_Language);
         Debug.Log("text.text : " + text.text);
-        value = text.text.Split('\n');
+        string[]  value = text.text.Split('\n');
         for (int i = 0; i < value.Length; i ++)
         {
             text_Tutorial[i].text = value[i];
         }
+        CloseLangauge();
+    }
+
+    public void OpenLangauge()
+    {
+        delegate_Script.Scale_Open(() => CloseParent(true));
+    }
+    public void CloseLangauge()
+    {
+        delegate_Script.Scale_Close(()=>CloseParent(false)); 
+    }
+    void CloseParent(bool check)
+    {
+        ui_Langauge.transform.parent.gameObject.SetActive(check);
     }
 }
